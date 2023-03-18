f33ea04455ea12ec4147d5794b73eea1a4b108c3
Cameron Shannon <cshannon@choicepays.com>
2023-03-18 15:46:34 -0700

    Added allowed hosts for new cname

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="uufI6MwGUxZwzbn6khanc9wFbDuHOERMbKClQWKEaMrmDkT5A7IbJk4Lbas6YuIj",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "thewalkupeffect.org"]
 
 # CACHES
 # ------------------------------------------------------------------------------
