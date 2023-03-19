95d1e43a66c8bbe78a4bc0945db3860d4b811a7d
Cameron Shannon <cshannon@choicepays.com>
2023-03-18 18:12:24 -0700

    Updated ALLOWED_HOSTS

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,7 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS",
-                         default=["thewalkupeffect.org", "www.thewalkupeffect.org", "thewalkupeffect.herokuapp.com"])
+                         default=[".thewalkupeffect.org", "thewalkupeffect.herokuapp.com"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
